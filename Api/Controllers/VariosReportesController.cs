2f6c415c6596160ee2c51d3dd9e6baf71c3e4343
evy.gaby <gestrada@uedelta.edu.ec>
2025-08-18 15:25:21 -0500

    Cambios 18/8

--- Api/Controllers/VariosReportesController.cs
+++ Api/Controllers/VariosReportesController.cs
@@ -12,17 +12,17 @@ namespace Api.Controllers
 {
     [Route("/[controller]/[action]")]
     [ApiController]
-    public class VariosReportesController:Controller
+    public class VariosReportesController : Controller
     {
         private readonly IDeltaContextProcedures _contextp;
-       
+
         public VariosReportesController(IDeltaContextProcedures deltaContextProcedures)
         {
 
             _contextp = deltaContextProcedures;
 
         }
-        
+
         [HttpGet]
         public async Task<IActionResult> ListaPeriodo(string usu, string pass)
 
@@ -38,14 +38,14 @@ namespace Api.Controllers
                          .ToDictionary(col => col.ColumnName, col => row[col]))
                      .OrderByDescending(dict => dict["PER_PERIODO"].ToString())
                      .ToList();
-            return Json(lista); 
+            return Json(lista);
         }
         [HttpGet]
-        public async Task<IActionResult> ListaSecciones(string usu, string pass,int idempresa,string periodo)
+        public async Task<IActionResult> ListaSecciones(string usu, string pass, int idempresa, string periodo)
 
         {
             TextoValor Lista = new TextoValor();
-            return Ok(_contextp.CallProceduresConsula(Lista, "rh_mantenimientos.QRY_SECCIONES_X_EMP('" + periodo + "','" + idempresa +"',:1)", usu, pass));
+            return Ok(_contextp.CallProceduresConsula(Lista, "rh_mantenimientos.QRY_SECCIONES_X_EMP('" + periodo + "','" + idempresa + "',:1)", usu, pass));
         }
         [HttpGet]
         public async Task<IActionResult> Orlas(string usu, string pass, int idempresa)
@@ -58,32 +58,50 @@ namespace Api.Controllers
         public async Task<IActionResult> Cumpleanios(string usu, string pass, int idempresa)
 
         {
-                        CumpleaniosPersonal Lista = new CumpleaniosPersonal();
-            return Ok(_contextp.CallProceduresConsula(Lista, "prock_personal_web.QRY_CUMPLE_EMPLEADOS("+ idempresa + ",null,null,:1)", usu, pass));
-       }
+            CumpleaniosPersonal Lista = new CumpleaniosPersonal();
+            return Ok(_contextp.CallProceduresConsula(Lista, "prock_personal_web.QRY_CUMPLE_EMPLEADOS(" + idempresa + ",null,null,:1)", usu, pass));
+        }
 
         [HttpGet]
         [Route("api/checklist")]
-        public async Task<IActionResult> NumAlumnos(string usu, string pass, string periodo, [FromQuery] List<string> niveles,string paralelo,string tipoRep)
+        public async Task<IActionResult> NumAlumnos(string usu, string pass, string periodo, [FromQuery] List<string> niveles, string paralelo, string tipoRep)
 
         {
-            
+
             NumAlumnosxSeccion Lista = new NumAlumnosxSeccion();
-            return Ok(_contextp.CallProceduresConsula(Lista, "PROC_K_REPORTES_ACADWEB.QRY_ReporteTotalAlumnas('" + periodo + "','"+ _contextp.SeccionesSeleccionadas( niveles) + "',null,'"+ tipoRep +"',:1)", usu, pass));
+            return Ok(_contextp.CallProceduresConsula(Lista, "PROC_K_REPORTES_ACADWEB.QRY_ReporteTotalAlumnas('" + periodo + "','" + _contextp.SeccionesSeleccionadas(niveles) + "',null,'" + tipoRep + "',:1)", usu, pass));
         }
         [HttpGet]
-        public async Task<IActionResult> ConsultarActas(string usu, string pass, string periodo, DateTime? desde,DateTime? hasta,string? vfiltro,Int32 codemp)
+        public async Task<IActionResult> ConsultaCapacitaciones(string usu, string pass, Int32 empresa, DateTime? desde, DateTime? hasta)
 
         {
-            var contextoOracle = new ModelOracleContext(); 
+            var contextoOracle = new ModelOracleContext();
             DeltaContextProcedures obj = new DeltaContextProcedures(contextoOracle);
             ///formato fechas
             var fdesde = desde.HasValue ? "'" + desde.Value.ToString("dd/MM/yyyy") + "'" : "null";
             var fhasta = hasta.HasValue ? "'" + hasta.Value.ToString("dd/MM/yyyy") + "'" : "null";
-            var ffiltro = ! string.IsNullOrWhiteSpace(vfiltro) ?  "'" + vfiltro + "'" : "null";
-            
+            var sentencia = "proc_k_rrhh_web.rpt_capacitaciones(" + empresa + "," + fdesde + "," + fhasta + ",:1)";
+            DataTable dt = obj.CallProceduresConsulaDT(sentencia, usu, pass);
+            var lista = dt.AsEnumerable()
+                     .Select(row => dt.Columns
+                         .Cast<DataColumn>()
+                         .ToDictionary(col => col.ColumnName, col => row[col]))
+                     .ToList();
+            return Json(lista); ;
+        }
+        [HttpGet]
+        public async Task<IActionResult> ConsultarActas(string usu, string pass, string periodo, DateTime? desde, DateTime? hasta, string? vfiltro, Int32 codemp)
 
-                var sentencia = "PROC_K_OTRASACTAS_WEB.QRY_Reporte_ActaRGP('" + periodo + "',12," + fdesde + "," + fhasta + "," + ffiltro + "," + codemp + ",'" + usu + "',:1)";
+        {
+            var contextoOracle = new ModelOracleContext();
+            DeltaContextProcedures obj = new DeltaContextProcedures(contextoOracle);
+            ///formato fechas
+            var fdesde = desde.HasValue ? "'" + desde.Value.ToString("dd/MM/yyyy") + "'" : "null";
+            var fhasta = hasta.HasValue ? "'" + hasta.Value.ToString("dd/MM/yyyy") + "'" : "null";
+            var ffiltro = !string.IsNullOrWhiteSpace(vfiltro) ? "'" + vfiltro + "'" : "null";
+
+
+            var sentencia = "PROC_K_OTRASACTAS_WEB.QRY_Reporte_ActaRGP('" + periodo + "',12," + fdesde + "," + fhasta + "," + ffiltro + "," + codemp + ",'" + usu + "',:1)";
             DataTable dt = obj.CallProceduresConsulaDT(sentencia, usu, pass);
             foreach (DataRow i in dt.Rows)
             {
@@ -100,14 +118,14 @@ namespace Api.Controllers
             return Json(lista); ;
         }
         [HttpGet]
-        public async Task<IActionResult> ActadeReunion(string usu, string pass,string periodo,int codigo)
+        public async Task<IActionResult> ActadeReunion(string usu, string pass, string periodo, int codigo)
 
         {
-           // return Ok(_contextp.ConsultarActa(usu, pass,periodo,codigo));
+            // return Ok(_contextp.ConsultarActa(usu, pass,periodo,codigo));
             //await new ("miUsuario", "miPass", "2025-08", 123);
             var contextoOracle = new ModelOracleContext();
             DeltaContextProcedures obj = new DeltaContextProcedures(contextoOracle);
-            DataSet dt = await obj.ConsultarActa(usu, pass,periodo,codigo);
+            DataSet dt = await obj.ConsultarActa(usu, pass, periodo, codigo);
             List<object> listaDeListas = new List<object>();
             var Cabecera = dt.Tables[0].AsEnumerable()
                      .Select(row => dt.Tables[0].Columns
@@ -128,7 +146,7 @@ namespace Api.Controllers
                      .ToList();
             listaDeListas.Add(OrdenDia);
 
-           
+
             var Adjuntos = dt.Tables[3].AsEnumerable()
                      .Select(row => dt.Tables[3].Columns
                          .Cast<DataColumn>()
@@ -137,15 +155,35 @@ namespace Api.Controllers
             listaDeListas.Add(Adjuntos);
             return Json(listaDeListas);
         }
-    }
+        [HttpGet]
+        public async Task<IActionResult> DatosExcel(string usu, string pass, Int32 empresa)
 
-    public class OrdenDia
-    {
+        {
+            var contextoOracle = new ModelOracleContext();
+            DeltaContextProcedures obj = new DeltaContextProcedures(contextoOracle);
+            var sentencia = "prock_personal_web.QRY_DATOS_EMPLEADO(" + empresa + ",:1)";
+            DataTable dt = obj.CallProceduresConsulaDT(sentencia, usu, pass);
+            var lista = dt.AsEnumerable()
+                     .Select(row => dt.Columns
+                         .Cast<DataColumn>()
+                         .ToDictionary(col => col.ColumnName, col => row[col]))
+                     .ToList();
+            return Json(lista); ;
+        }
+        [HttpGet]
+        public async Task<IActionResult> JefasArea(string usu, string pass, Int32 empresa)
 
-        public Int16? SEC_DETRG { get; set; }
-        public string? ORDENDIA { get; set; }
-        public string? DESARROLLO { get; set; }
-        public string? ACUERDOS { get; set; }
-        public Int16? SEC_CABRG { get; set; }
+        {
+            var contextoOracle = new ModelOracleContext();
+            DeltaContextProcedures obj = new DeltaContextProcedures(contextoOracle);
+            var sentencia = "PROC_K_RRHH_WEB.QRY_JefesArea("+ empresa +",:1)";
+            DataTable dt = obj.CallProceduresConsulaDT(sentencia, usu, pass);
+            var lista = dt.AsEnumerable()
+                     .Select(row => dt.Columns
+                         .Cast<DataColumn>()
+                         .ToDictionary(col => col.ColumnName, col => row[col]))
+                     .ToList();
+            return Json(lista); ;
+        }
     }
 }
